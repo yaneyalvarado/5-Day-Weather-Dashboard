b5f8bb4a6bd1e5673607956f388002f94a93ae6d
yaneyalvarado <alvyaney@gmail.com>
2023-03-29 20:50:37 -0600

    added new var in js

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,7 @@ searchButton.addClass(".btn btn-primary");
 searchButton.text(cityName);
 
 var apiKey = "1b18ce13c84e21faafb19c931bb29331"
+// obtain and use data from open weather current weather API end point
+var weatherInfo = function(cityName) {
+
+}
