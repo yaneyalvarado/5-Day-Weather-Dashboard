e53708e2ccebea1f9b58e2561088ded86403adc9
yaneyalvarado <alvyaney@gmail.com>
2023-03-29 21:18:05 -0600

    updated js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var searchButton = $('#search-button');
 var searchHistoryList = $('#search-history-list');
+var today = dayjs().format("MM, DD, YYYY")
 
 searchHistoryList = function (cityName) {
     $('.past-search:contains("' + cityName + '")').remove();
@@ -12,5 +13,10 @@ searchButton.text(cityName);
 var apiKey = "1b18ce13c84e21faafb19c931bb29331"
 // obtain and use data from open weather current weather API end point
 var weatherInfo = function(cityName) {
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}')
+  // obtain response and convert to objects
+  .then(function(response) {
+    return response.json();
+  })
 
-}
+} 
